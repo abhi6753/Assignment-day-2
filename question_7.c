858bd339e0ff28d4cfd586c3a67d1a6385789134
Abhishek kumar <113863385+abhi6753@users.noreply.github.com>
2025-03-28 22:17:34 +0530

    Update question_7.c

--- question_7.c
+++ question_7.c
@@ -3,13 +3,18 @@
 #include<stdio.h>
 int main()
 {
-    int num;
-    printf("Enter a number:\n");
+    int num,lsb,count=1;
+    printf("Enter a number to find the position of first 1 from LSB:\n");
     scanf("%d",&num);
-    if(num%2 == 0)
-      printf("Least significant bit is 0.");
-    else
-      printf("Least significant bit is 1.");
+    for( ; ; )
+    {
+         lsb = num&1;
+        if(lsb)
+            break;
+        num>>=1;
+        count++;
+    }
+   printf(" first 1 from lSB is at %d position",count);
     getch();
     return 0;
 }
